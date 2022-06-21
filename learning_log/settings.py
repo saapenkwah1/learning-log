74c1eada78bf50beb1f9f987892bf1570bf00d8a
saapenkwah1 <stephenannor648@gmail.com>
2022-06-21 13:23:58 +0000

    Returned back

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -137,7 +137,7 @@ import django_heroku
 django_heroku.settings(locals())
 
 #DEBUG CHECK
-if bool(os.environ.get('DEBUG')) == True:
+if os.environ.get('DEBUG') == 'TRUE':
     DEBUG = True
-elif bool(os.environ.get('DEBUG')) == False:
+elif bool(os.environ.get('DEBUG')) == 'FALSE':
     DEBUG = False
