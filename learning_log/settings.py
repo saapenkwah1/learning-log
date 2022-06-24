bdffb2f23e6bf2356bf2268259a8e1da0ccc82e4
saapenkwah1 <stephenannor648@gmail.com>
2022-06-24 19:38:37 +0000

    Secured my Secret key

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -21,7 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3dm5r%bk*bz2jn4+d_ur-j07tgiq&7!dliqon6!6xgs&65uh$-'
+from decouple import config
+
+SECRET_KEY = config("SECRET_KEY") # this is to replace the secret key you cut away before
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
